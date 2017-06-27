5ce0b3c49bf4d2896aafd7a40f1e9a624e0c6aa6
Eric Fischer <enf@pobox.com>
2017-06-27 16:21:12 -0700

    Matching against count files seems to work

--- grep.cpp
+++ grep.cpp
@@ -16,38 +16,58 @@ extern "C" {
 }
 
 bool quiet = false;
+unsigned long long mask = 0;
 
 void usage(char **argv) {
 	fprintf(stderr, "Usage: %s [-v] [-s binsize] in.count [in.json ...]\n", argv[0]);
 }
 
-void write_point(FILE *out, long long &seq, double lon, double lat, unsigned long long count) {
-	if (seq % 100000 == 0) {
-		if (!quiet) {
-			fprintf(stderr, "Read %.1f million records\r", seq / 1000000.0);
+int maskcmp(const void *a, const void *b) {
+	unsigned char *ca = (unsigned char *) a;
+	unsigned char *cb = (unsigned char *) b;
+
+	unsigned long la = read64(ca) & mask;
+	unsigned long lb = read64(cb) & mask;
+
+	if (la < lb) {
+		return -1;
+	} else if (la > lb) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+bool check_coordinates(json_pull *jp, json_object *j, json_object *coordinates, const char *map, size_t maplen) {
+	if (coordinates->type == JSON_ARRAY) {
+		if (coordinates->length >= 2 && coordinates->array[0]->type == JSON_NUMBER &&
+		    coordinates->array[1]->type == JSON_NUMBER) {
+			long long x, y;
+			projection->project(coordinates->array[0]->number, coordinates->array[1]->number, 32, &x, &y);
+			unsigned long long index = encode(x, y);
+
+			unsigned char ix[INDEX_BYTES];
+			unsigned char *p = ix;
+			write64(&p, index);
+
+			void *found = bsearch(ix, map + HEADER_LEN, (maplen - HEADER_LEN) / RECORD_BYTES, RECORD_BYTES, maskcmp);
+
+			if (found != NULL) {
+				return true;
+			}
+		} else {
+			for (size_t i = 0; i < coordinates->length; i++) {
+				if (check_coordinates(jp, j, coordinates->array[i], map, maplen)) {
+					return true;
+				}
+			}
 		}
 	}
-	seq++;
 
-	long long x, y;
-	projection->project(lon, lat, 32, &x, &y);
-	unsigned long long index = encode(x, y);
-
-	while (count > MAX_COUNT) {
-		write64(out, index);
-		write32(out, MAX_COUNT);
-
-		count -= MAX_COUNT;
-	}
-
-	write64(out, index);
-	write32(out, count);
+	return false;
 }
 
-void check_coordinates(json_pull *jp, json_object *j, json_object *coordinates) {
-}
-
-void read_json(FILE *in, const char *fname, long long &seq) {
+void read_json(FILE *in, const char *fname, long long &seq, const char *map, size_t maplen, bool reverse) {
 	json_pull *jp = json_begin_file(in);
 
 	while (1) {
@@ -88,7 +108,19 @@ void read_json(FILE *in, const char *fname, long long &seq) {
 			continue;
 		}
 
-		check_coordinates(jp, j, coordinates);
+		if (seq % 100000 == 0) {
+			if (!quiet) {
+				fprintf(stderr, "Read %.1f million records\r", seq / 1000000.0);
+			}
+		}
+		seq++;
+
+		if (check_coordinates(jp, j, coordinates, map, maplen) != reverse) {
+			const char *out = json_stringify(j);
+			printf("%s\n", out);
+			free((void *) out);
+		}
+
 		json_free(j);
 	}
 
@@ -104,7 +136,7 @@ int main(int argc, char **argv) {
 	size_t cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
 	int i;
-	while ((i = getopt(argc, argv, "s:v")) != -1) {
+	while ((i = getopt(argc, argv, "s:vq")) != -1) {
 		switch (i) {
 		case 's':
 			zoom = atoi(optarg);
@@ -112,6 +144,10 @@ int main(int argc, char **argv) {
 
 		case 'v':
 			reverse = true;
+			break;
+
+		case 'q':
+			quiet = true;
 			break;
 
 		default:
@@ -125,12 +161,34 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+        if (zoom != 0) {
+                mask = 0xFFFFFFFFFFFFFFFFULL << (64 - 2 * zoom);
+        }
+
 	const char *countfname = argv[optind];
+	int fd = open(countfname, O_RDONLY);
+	if (fd < 0) {
+		perror(countfname);
+		exit(EXIT_FAILURE);
+	}
+
+	struct stat st;
+	if (fstat(fd, &st) != 0) {
+		perror(countfname);
+		exit(EXIT_FAILURE);
+	}
+
+	const char *map = (const char *) mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (map == MAP_FAILED) {
+		perror(countfname);
+		exit(EXIT_FAILURE);
+	}
+
 	optind++;
 
 	long long seq = 0;
 	if (optind == argc) {
-		read_json(stdin, "standard input", seq);
+		read_json(stdin, "standard input", seq, map, st.st_size, reverse);
 	} else {
 		for (; optind < argc; optind++) {
 			FILE *in = fopen(argv[optind], "r");
@@ -138,7 +196,7 @@ int main(int argc, char **argv) {
 				perror(argv[optind]);
 				exit(EXIT_FAILURE);
 			} else {
-				read_json(in, argv[optind], seq);
+				read_json(in, argv[optind], seq, map, st.st_size, reverse);
 				fclose(in);
 			}
 		}
