a0824b5a9ed45935eb2dd5a9c99b92e2054eed78
Eric Fischer <enf@pobox.com>
2017-11-27 13:45:59 -0800

    Add a sanity check in binary search for the list being out of sort

--- merge.cpp
+++ merge.cpp
@@ -132,6 +132,48 @@ void *run_merge(void *va) {
 	return NULL;
 }
 
+// Inlined copy of std::lower_bound, with an additional check added
+// for the impossible situation where the length of the list being
+// searched goes negative.
+
+template <class _Compare, class _ForwardIterator, class _Tp>
+_ForwardIterator
+__lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_, _Compare __comp) {
+	typedef typename std::iterator_traits<_ForwardIterator>::difference_type difference_type;
+	difference_type __len = std::__1::distance(__first, __last);
+	while (__len > 0) {
+		difference_type __l2 = __len / 2;
+		_ForwardIterator __m = __first;
+		std::__1::advance(__m, __l2);
+		if (__comp(*__m, __value_)) {
+			__first = ++__m;
+			__len -= __l2 + 1;
+		} else
+			__len = __l2;
+	}
+	if (__len < 0) {
+		fprintf(stderr, "Error: Input file is out of sort\n");
+		exit(EXIT_FAILURE);
+	}
+	return __first;
+}
+
+template <class _ForwardIterator, class _Tp, class _Compare>
+inline __attribute__((__visibility__("hidden"), __always_inline__))
+_ForwardIterator
+lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_, _Compare __comp) {
+	typedef typename std::add_lvalue_reference<_Compare>::type _Comp_ref;
+	return __lower_bound1<_Comp_ref>(__first, __last, __value_, __comp);
+}
+
+template <class _ForwardIterator, class _Tp>
+inline __attribute__((__visibility__("hidden"), __always_inline__))
+_ForwardIterator
+lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_) {
+	return lower_bound1(__first, __last, __value_,
+			    std::__less<typename std::iterator_traits<_ForwardIterator>::value_type, _Tp>());
+}
+
 void do_merge(struct merge *merges, size_t nmerges, int f, int bytes, long long nrec, int zoom, bool quiet, size_t cpus, size_t also_todo, size_t also_did) {
 	unsigned long long mask = 0;
 	if (zoom != 0) {
@@ -214,7 +256,7 @@ void do_merge(struct merge *merges, size_t nmerges, int f, int bytes, long long 
 			unsigned char *p = look.data;
 			write64(&p, beginning[i] & mask);
 
-			finder *l = std::lower_bound(fs, fe, look);
+			finder *l = lower_bound1(fs, fe, look);
 
 			merger m;
 			m.start = (unsigned char *) l;
