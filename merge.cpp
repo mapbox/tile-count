32fff6779a066409527648be04b24f74dd4a6402
Eric Fischer <enf@pobox.com>
2017-11-27 14:19:22 -0800

    Clean up modifications to binary search

--- merge.cpp
+++ merge.cpp
@@ -10,6 +10,7 @@
 #include "merge.hpp"
 #include "header.hpp"
 #include "serial.hpp"
+#include "algorithm_mod.hpp"
 
 struct merger {
 	unsigned char *start;
@@ -130,48 +131,6 @@ void *run_merge(void *va) {
 	a->outlen = end - (a->out + a->off);
 
 	return NULL;
-}
-
-// Inlined copy of std::lower_bound, with an additional check added
-// for the impossible situation where the length of the list being
-// searched goes negative.
-
-template <class _Compare, class _ForwardIterator, class _Tp>
-_ForwardIterator
-__lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_, _Compare __comp) {
-	typedef typename std::iterator_traits<_ForwardIterator>::difference_type difference_type;
-	difference_type __len = std::__1::distance(__first, __last);
-	while (__len > 0) {
-		difference_type __l2 = __len / 2;
-		_ForwardIterator __m = __first;
-		std::__1::advance(__m, __l2);
-		if (__comp(*__m, __value_)) {
-			__first = ++__m;
-			__len -= __l2 + 1;
-		} else
-			__len = __l2;
-	}
-	if (__len < 0) {
-		fprintf(stderr, "Error: Input file is out of sort\n");
-		exit(EXIT_FAILURE);
-	}
-	return __first;
-}
-
-template <class _ForwardIterator, class _Tp, class _Compare>
-inline __attribute__((__visibility__("hidden"), __always_inline__))
-_ForwardIterator
-lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_, _Compare __comp) {
-	typedef typename std::add_lvalue_reference<_Compare>::type _Comp_ref;
-	return __lower_bound1<_Comp_ref>(__first, __last, __value_, __comp);
-}
-
-template <class _ForwardIterator, class _Tp>
-inline __attribute__((__visibility__("hidden"), __always_inline__))
-_ForwardIterator
-lower_bound1(_ForwardIterator __first, _ForwardIterator __last, const _Tp &__value_) {
-	return lower_bound1(__first, __last, __value_,
-			    std::__less<typename std::iterator_traits<_ForwardIterator>::value_type, _Tp>());
 }
 
 void do_merge(struct merge *merges, size_t nmerges, int f, int bytes, long long nrec, int zoom, bool quiet, size_t cpus, size_t also_todo, size_t also_did) {
